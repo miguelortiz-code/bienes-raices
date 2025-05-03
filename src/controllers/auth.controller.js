fdc833ea135676d860ea8dc5a6c4b99d669068f1
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-05-03 15:29:27 -0500

    feat(ui): create recover password page and style it along with login and register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,23 @@
 const formularioLogin  =  (req, res) =>{
     res.render('auth/login', {
-         
+        pagina: 'Iniciar Sesión'
     });
 }
 
 const formularioRegister  =  (req, res) =>{
     res.render('auth/register', {
-         
+        pagina: 'Crear Cuenta'
+    });
+}
+
+const formularioRecoveryPassword  =  (req, res) =>{
+    res.render('auth/recover-password', {
+        pagina: 'Recuperar  Contraseña'
     });
 }
 
 export {
     formularioLogin,
-    formularioRegister
+    formularioRegister,
+    formularioRecoveryPassword
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { formularioLogin, formularioRegister } from '../controllers/auth.controller.js';
+import { formularioLogin, formularioRegister, formularioRecoveryPassword } from '../controllers/auth.controller.js';
 const router =  express.Router();
 
 router.get('/login', formularioLogin);
-
 router.get('/register', formularioRegister)
+router.get('/recover-password', formularioRecoveryPassword)
 
 export default router;
