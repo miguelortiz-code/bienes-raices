87db5efb862cabf6d32a37c623d9ac22353d00bd
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-07-10 13:14:36 -0500

    feat(ui): implement responsive filters with dynamic category and price options from database.

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,6 +1,16 @@
-const home = (req, res) =>{
+import { raw } from 'mysql2';
+import {Categories, Prices, Properties} from  '../models/index.js';
+
+const home = async (req, res) =>{
+    const [categories, prices] = await Promise.all([
+        Categories.findAll({raw: true}),
+        Prices.findAll({raw: true})
+    ]);
+    console.log(categories);
     res.render('home', {
-        pagina: 'Inicio'
+        pagina: 'Inicio',
+        categories,
+        prices
     })
 };
 
