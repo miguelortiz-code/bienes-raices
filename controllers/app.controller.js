fa8a193f1c01d0fe2f774ad482d5bd1ac08e0518
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-07-10 15:54:00 -0500

    feat(ui): implement "houses for sale" section on homepage with dynamic title, price, rooms, and link to property view.

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,16 +1,43 @@
-import { raw } from 'mysql2';
 import {Categories, Prices, Properties} from  '../models/index.js';
 
 const home = async (req, res) =>{
-    const [categories, prices] = await Promise.all([
+    const [categories, prices, houses, apartaments] = await Promise.all([
         Categories.findAll({raw: true}),
-        Prices.findAll({raw: true})
+        Prices.findAll({raw: true}),
+        Properties.findAll({
+            limit: 3,
+            where: {
+                id_category: 1
+            },
+            include :[
+                {
+                   model: Prices, 
+                   as: 'price'
+                }
+            ],
+            order: [['createdAt', 'DESC']]
+        }),
+        Properties.findAll({
+            limit: 3,
+            where: {
+                id_category: 2
+            },
+            include :[
+                {
+                   model: Prices, 
+                   as: 'price'
+                }
+            ],
+            order: [['createdAt', 'DESC']]
+        }),
     ]);
-    console.log(categories);
+
     res.render('home', {
         pagina: 'Inicio',
         categories,
-        prices
+        prices,
+        houses,
+        apartaments
     })
 };
 
