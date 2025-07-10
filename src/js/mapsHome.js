1913735a05ad756b2f19b8624c432121e576269c
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-07-10 12:38:46 -0500

    feat(ui): consume properties API and display map pins with image, title, price, and link to detail view.

--- src/js/mapsHome.js
+++ src/js/mapsHome.js
@@ -2,7 +2,7 @@
   const lat = 4.651002988522;
   const lng = -74.081282901615;
   const maps = L.map("maps-home").setView([lat, lng], 12);
-  
+ let markers = new L.FeatureGroup().addTo(maps); 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -13,11 +13,30 @@
       const url = '/api/properties';
       const reply = await fetch(url)
       const properties = await reply.json();
-      console.log(properties);
+      showProperties(properties);
     } catch (error) {
       console.log(error);
     }
   };
+
+  const showProperties = properties =>{
+    properties.forEach(property =>{
+      // Agregar Pines
+      const marker = new L.marker([property?.latitude, property?.longitude],{
+        autoPan: true
+      })
+      .addTo(maps)
+      .bindPopup(`
+        <p class="text-blue-600">${property.category.category}</p>
+        <h1 class="text-center text-xl font-extrabold uppercase my-2">${property.title}</h1>
+        <img src="/uploads/${property?.imagen}" alt="Imagen de la propiedad: ${property?.title}" />
+        <p class="text-center font-bold text-gray-600">${property.price.price}</p>
+        <a href="/property/${property.code}" target="_blank" class="link block bg-blue-600 text-center rounded-lg p-2 uppercase">ver propiedad</a>
+      `);
+
+      markers.addLayer(marker)
+    });
+  }
   obtainProperties()
 
 }())
