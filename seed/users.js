e15c66501cb20db36d13658ecbda4a8b15218b04
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-07-14 20:14:45 -0500

    chore(core): add two test users in production user seeder.

--- seed/users.js
+++ seed/users.js
@@ -15,6 +15,22 @@ const users = [
         token: null,
         confirmed: 1
     },
+    // usuarios de prueba
+    {
+        name: 'Prueba',
+        email: 'prueba@gmail.com',
+        password: bcrypt.hashSync('Prueba123', 10),
+        token: null,
+        confirmed: 1
+    },
+
+    {
+        name: 'Prueba 2',
+        email: 'correo@gmail.com',
+        password: bcrypt.hashSync('Prueba123*', 10),
+        token: null,
+        confirmed: 1
+    },
 ]
 
 export default users
